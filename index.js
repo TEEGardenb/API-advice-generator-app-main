700984ec43df4412eac2a9a2f20b5c880147874b
Mateo Parra <mateoparra1171@gmail.com>
2023-07-05 16:09:23 -0500

    API-advice-generator-app-main

---
+++ index.js
@@ -0,0 +1,16 @@
+let Button = document.getElementById("button");
+let CounterVariable = document.getElementById("counter-varible");
+let ContainerText = document.getElementById("text");
+
+//Evento al hacer click en el boton
+Button.onclick = function () {
+  fetch("https://api.adviceslip.com/advice", {})
+    .then((response) => response.json())
+    .then((data) => {
+      const ID = data.slip.id;
+      const Text_1 = data.slip.advice;
+
+      CounterVariable.innerHTML = ID;
+      ContainerText.innerHTML = Text_1;
+    });
+};
